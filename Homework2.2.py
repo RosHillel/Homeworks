d956000f1352923c167d47acfaf4d60b682c3db7
Rostyslav <rostislav@gmail.com>
2025-02-28 20:57:23 +0100

    Revert "Homework task2.2 commit"
    This reverts commit ff6866d43686c2a481fbe4141478019177d35e9f.

--- Homework2.2.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Введіть 5ти значне число: ")) #12345
-n1 = number % 10
-n2 = number % 100 // 10
-n3 = number % 1000 // 100
-n4 = number % 10000 // 1000
-n5 = number // 10000
-
-print(n1, n2, n3, n4, n5, sep="\n")
