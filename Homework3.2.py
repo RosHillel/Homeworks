f46de0467380e7ad1d833bbced119e6c8b9a50b2
Rostyslav <rostislav@gmail.com>
2025-03-05 16:01:14 +0100

    first saving

---
+++ Homework3.2.py
@@ -0,0 +1,9 @@
+numbers = [14, 3, 1, 7, 4, 2]
+if len(numbers) > 0:
+    numbers.insert(0, numbers[-1])
+    print(numbers[:-1])  # або  numbers.pop()
+#                               print(numbers)
+
+
+
+
